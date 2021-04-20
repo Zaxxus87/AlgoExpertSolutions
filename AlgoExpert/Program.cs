2a027752ad3d44f786e5e068d40540eedeb35275
Jeffrey Baker <jeffrey1871@gmail.com>
2021-04-19 22:36:09 -0500

    initial commit

---
+++ AlgoExpert/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+   static void Main(string[] args)
+   {
+		string output = "[";
+		var ray = InsertionSort(new int[] { 8, 5, 2, 9, 5, 6, 3 });
+		foreach(int num in ray)
+        {
+			output += $"{num}, ";
+        }
+		output = output.TrimEnd(',') + "]";
+        Console.WriteLine(output);
+
+	}
+
+	public static int[] InsertionSort(int[] array)
+	{
+		for (int i = 0; i < array.Length - 1; i++)
+		{
+			int j = i + 1;
+			int temp = array[j];
+			while (j > 0 && temp < array[j - 1])
+			{
+				array[j] = array[j - 1];
+				j--;
+			}
+			array[j] = temp;
+		}
+		return array;
+	}
+}
